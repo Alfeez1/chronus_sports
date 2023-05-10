b5898b7bce4481522eb438c10893cde7993159ce
Alfeez1 <Muhammed.alfeez@b-jelt.com>
2023-05-10 14:06:48 +0530

    marketplace section completed

---
+++ src/constants/filter.ts
@@ -0,0 +1,66 @@
+const filterData = [
+  {
+    id: 1,
+    name: 'Blue Chipes',
+  },
+  {
+    id: 2,
+    name: 'Preço',
+  },
+  {
+    id: 3,
+    name: 'Esportes',
+    subcategories: [
+      {
+        id: 1,
+        name: 'Artes Marciais',
+        subcategories: [
+          {
+            id: 1,
+            name: 'Jiu-Jitsu',
+          },
+          {
+            id: 2,
+            name: 'Judo',
+          },
+          {
+            id: 3,
+            name: 'MMA',
+          },
+        ],
+      },
+      {
+        id: 2,
+        name: 'Basquete',
+      },
+      {
+        id: 3,
+        name: 'Boxe',
+      },
+      {
+        id: 4,
+        name: 'Futebol',
+        subcategories: [
+          {
+            id: 1,
+            name: 'Calabresa',
+          },
+        ],
+      },
+      {
+        id: 5,
+        name: 'Futsal',
+      },
+      {
+        id: 6,
+        name: 'Natação',
+        subcategories: [
+          {
+            id: 1,
+            name: 'Calabresa',
+          },
+        ],
+      },
+    ],
+  },
+];
